ee9169826373f93b5c1f8c427053f7045ea8abd2
darveaujp <53666229+darveaujp@users.noreply.github.com>
2019-08-04 13:16:36 -0400

    adding build folder

---
+++ hackernews/build/precache-manifest.a72904936b4f45faf0e940ceb28077c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0866c1e51f655cf3fae2cc765a21b8ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da55ba72a1f0f2db7e8",
+    "url": "/static/css/main.f5ae9cfd.chunk.css"
+  },
+  {
+    "revision": "5042f8ba3c723d84772f",
+    "url": "/static/js/2.9039306b.chunk.js"
+  },
+  {
+    "revision": "3da55ba72a1f0f2db7e8",
+    "url": "/static/js/main.62438ada.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
